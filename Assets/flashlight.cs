45542f683aa5d40e2ffd1603c7464e309bcec764
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-10-15 12:25:04 -0600

    initial commit, flashlight, movement, enclosure

---
+++ Assets/flashlight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flashlight : MonoBehaviour {
+
+//	public Light flashlight;
+	public double power = 100.0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Light> ().enabled == true) {
+			power = power - .1;
+		} else {
+			if (power <= 99.9) {
+				power = power + .1;
+			}
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("hi");
+			if (GetComponent<Light> ().enabled == true) {
+				GetComponent<Light> ().enabled = false; 
+			} else {
+				GetComponent<Light> ().enabled = true; 
+			}
+		}
+	}
+	
+}
