65dde1878ed9eb519adeb846d22c35e85b07e1b5
Hassan Shaikley <hassan.shaikley@gmail.com>
2018-01-23 12:50:22 -0700

    quit on esc; nicer menu

---
+++ Assets/quit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update() {
+		if (Input.GetKey("escape"))
+			Application.Quit();
+
+	}
+}
